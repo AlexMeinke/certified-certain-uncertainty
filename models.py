21d9088b8a3f8fc105cc7c73d70a333f9a00cbb4
Alex Meinke <alexpw@ymail.com>
2019-04-02 10:07:36 +0000

    added preprocessing to LeNet CNN

--- models.py
+++ models.py
@@ -136,14 +136,17 @@ class GMM(nn.Module):
 
 
 class NetVanilla(nn.Module):
-    def __init__(self):
+    def __init__(self, preproc=torch.zeros(28, 28)):
         super(NetVanilla, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 5, 1)
         self.conv2 = nn.Conv2d(32, 64, 5, 1)
         self.fc1 = nn.Linear(4*4*64, 1024)
         self.fc2 = nn.Linear(1024, 10)
+        self.preproc = nn.Parameter(preproc[0], requires_grad=False)
+        self.preproc_std = nn.Parameter(preproc[1], requires_grad=False)
 
     def forward(self, x):
+        x = (x-self.preproc)/self.preproc_std
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.conv2(x))
@@ -153,6 +156,7 @@ class NetVanilla(nn.Module):
         x = self.fc2(x)
         x = F.log_softmax(x, dim=1)
         return x
+
 
 class Net(nn.Module):
     def __init__(self, base_model, X, K, loglam, dim=784):
